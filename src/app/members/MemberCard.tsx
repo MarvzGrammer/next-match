03b67538620a316c4aebc44ae98b4489685fd732
Marvei.Pana <nkcc12345@gmail.com>
2024-06-04 20:11:36 +0800

    End of section 5

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import LikeButton from '@/components/LikeButton'
 import { calculateAge } from '@/lib/util'
 import { Card, CardFooter, Image } from '@nextui-org/react'
 import { Member } from '@prisma/client'
@@ -6,9 +9,16 @@ import React from 'react'
 
 type Props = {
     member: Member
+    likeIds: string[]
 }
 
-export default function MemberCard({member}: Props) {
+export default function MemberCard({member, likeIds}: Props) {
+    const hasLiked = likeIds.includes(member.userId);
+
+    const preventLinkAction = (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+    }
   return (
     <Card 
         fullWidth
@@ -23,6 +33,12 @@ export default function MemberCard({member}: Props) {
             src={member.image || '/image/user.png'}
             className='aspect-square object-cover'
         />
+        <div onClick={preventLinkAction}>
+            <div className='absolute top-3 right-3 z-50'>
+                <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+            </div>
+        </div>
+        
         <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
             <div className='flex flex-col text-white'>
                 <span className='font-semibold'>{member.name}, {calculateAge(member.dateOfBirth)}</span>
