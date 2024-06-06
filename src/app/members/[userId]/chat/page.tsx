1f89a9e8d3fc2b57c03296829bcecc4541fd2ee7
Marvei.Pana <nkcc12345@gmail.com>
2024-06-07 06:49:57 +0800

    End of section 7

--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -1,16 +1,31 @@
-import { CardBody, CardHeader, Divider } from '@nextui-org/react'
+import CardInnerWrapper from '@/components/CardInnerWrapper'
 import React from 'react'
+import ChatForm from './ChatForm'
+import { getMessageThread } from '@/app/actions/messageActions'
+import MessageBox from './MessageBox';
+import { getAuthUserId } from '@/app/actions/authActions';
 
-export default function ChatPage() {
+export default async function ChatPage({params}: {params: {userId: string}}) {
+  const userId = await getAuthUserId();
+  const messages = await getMessageThread(params.userId);
+
+  const body = (
+    <div>
+      {messages.length === 0 ? "No messages to display": (
+        <div>
+          {messages.map(message => (
+            <MessageBox key={message.id} message={message} currentUserId={userId} />
+          ))}
+        </div>
+      )}
+    </div>
+  )
+  console.log(messages)
   return (
-    <>
-        <CardHeader className='text-2xl font-semibold text-secondary'>
-          Chat
-        </CardHeader>
-        <Divider />
-        <CardBody>
-            Chat go here
-        </CardBody>
-    </>
+    <CardInnerWrapper 
+      header='Chat'
+      body={body}
+      footer={<ChatForm />}
+    />
   )
 }
